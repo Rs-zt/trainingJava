cdb9c164ea1e4bf599d84042d50d46a01d12cc64
Rs-zt <2031958660@qq.com>
2023-08-08 10:18:42 +0800

    文件夹的创建和删除

--- src/teach/aistar/day17/FileDemo.java
+++ src/teach/aistar/day17/FileDemo.java
@@ -14,11 +14,38 @@ public class FileDemo {
     public static void main(String[] args) {
         String uuid = UUID.randomUUID().toString();
         String uu = uuid + ".txt";
-        createFile("E:\\CX_peixun\\demo3\\src\\teach\\aistar\\day17\\" + uu);
-//        createFolder("src\\teach\\aistar\\day17\\");
+//        createFile("E:\\CX_peixun\\demo3\\src\\teach\\aistar\\day17\\" + uu);
+//        createFolder("src\\teach\\aistar\\day17\\aa\\b\\c");
+        deleteFileFolder("src\\teach\\aistar\\day17\\aa\\b\\c");
+    }
+
+    private static void deleteFileFolder(String s) {
+        File file = new File(s);
+        if (!file.exists()) {
+            System.out.println(file + "不存在");
+            return;
+        }
+        //如果是文件实例,可以直接删除
+        if (file.delete()){
+            System.out.println(file + "删除成功");
+        }else {
+            System.out.println(file + "删除失败");
+        }
+
     }
 
     private static void createFolder(String folder) {
+        File file = new File(folder);
+        if (file.exists()) {
+            System.out.println("sorry,"+file+"已经存在");
+            return;
+        }
+        //创建多个目录
+        if (file.mkdirs()){
+            System.out.println(file + "创建成功");
+        }else {
+            System.out.println("创建失败");
+        }
     }
 
     private static void createFile(String s) {
